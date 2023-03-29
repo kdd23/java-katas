b2272870cf68409140e51120171eae675611e795
christ lambert <yyc@hotmail.de>
2023-03-29 21:43:39 +0200

    refactoring..

--- src/test/java/org/root/javakatas/codes/BitcountingTest.java
+++ src/test/java/org/root/javakatas/codes/BitcountingTest.java
@@ -1,6 +1,7 @@
 package org.root.javakatas.codes;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,24 +12,29 @@ public class BitcountingTest {
         assertEquals(2, Bitcounting.countBits(10));
     }
 
+    @Test
     public void testGame1234 () {
         assertEquals(5, Bitcounting.countBits(1234));
     }
 
+    @Test
     public void testGame4 () {
         assertEquals(1, Bitcounting.countBits(4));
     }
 
+    @Test
     public void testGame9 () {
         assertEquals(2, Bitcounting.countBits(9));
     }
 
+    @Test
     public void testGame7 () {
         assertEquals(3, Bitcounting.countBits(7));
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void testNegativeCounting () {
-        assertEquals(2, Bitcounting.countBits(-10));
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> Bitcounting.countBits(-10), "Negative numbers not allowed");
+        Assertions.assertNotEquals(2, exception.getMessage());
     }
 }
